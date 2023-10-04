c036357116a0e4f92ebb5e7b6c8ee3c9349fbb97
legagain <legagain@wisenut.co.kr>
2023-10-04 22:08:07 +0900

    :sparkles: feat: add draft of response model with pydantic basemodel
    - pydantic의 basemodel에 기반하여 출퇴근 API req/res 형식 정의 및 models.py draft 구현

--- app/models.py
+++ app/models.py
@@ -2,13 +2,31 @@ from typing import Any
 
 from pydantic import BaseModel
 
-from app import SERVICE_CODE, Log
+from app import Log
 from app.version import VERSION
 
 
 class APIResponseModel(BaseModel):
-    """기본 API 응답 포맷 by AIP Restful API 디자인 가이드"""
-    code: int = int(f"{SERVICE_CODE}200")
+    """기본 API 응답 포맷 by free-work-squad"""
+    code: int = -1
     message: str = f"API Response Success ({VERSION})" if Log.is_debug_enable() else "API Response Success"
     result: dict[str, Any] = {}
-    description: str = ""
+
+
+class AttendanceCheckResponse(BaseModel):
+    """출근 신청 결과 포맷"""
+    check_in_time: str  # 출근 시간을 문자열로 저장 (예: "2023-10-04 09:00:00")
+    login_id: str  # 로그인 ID 정보를 문자열로 저장
+    gps_info: str  # GPS 정보를 문자열로 저장 (예: "Latitude: 37.1234, Longitude: 128.5678")
+
+
+class AttendanceCheckInAPIResponseModel(APIResponseModel):
+    """출근 신청 API 응답 포맷 by free-work-squad"""
+    message: str = f"출근 신청 성공"
+    result: AttendanceCheckResponse
+
+
+class AttendanceCheckOutAPIResponseModel(APIResponseModel):
+    """퇴근 신청 API 응답 포맷 by free-work-squad"""
+    message: str = f"퇴근 신청 성공"
+    result: AttendanceCheckResponse
