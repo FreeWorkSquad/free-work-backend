a1673f2d73c4be55703632419b344fde502204ee
Jude0124 <legagain@gmail.com>
2023-10-23 00:10:48 +0900

    :recycle: refactor: refactoring attendance API
    - 출/퇴근 API 요청 인스턴스 재사용하도록 싱글톤 패턴으로 변경
    - config  경로 수정

--- app/src/attendance/attendance_check.py
+++ app/src/attendance/attendance_check.py
@@ -1,5 +1,3 @@
-from typing import Dict, Union, Any, Set
-
 from timezonefinder import TimezoneFinder
 from pydantic import constr
 
@@ -16,82 +14,86 @@ import pytz
 
 class AttendanceCheck:
     """AttendanceCheck: 출퇴근 신청 클래스"""
-    DEFAULT_ACCOUNT_ID: constr = "freeworksquad"
-    DEFAULT_COORDINATE: Coordinate = Coordinate(latitude=0.0, longitude=0.0)
+    account_id: constr = "freeworksquad"
+    coordinate: Coordinate = Coordinate(latitude=0.0, longitude=0.0)
+    _instance = None
 
     def __init__(
             self,
-            account_id: constr,
-            coordinate: Coordinate,
             check_in_time_validation_strategy: TimeValidationStrategy = CheckInTimeValidationStrategyDefault(),
             check_out_time_validation_strategy: TimeValidationStrategy = CheckOutTimeValidationStrategyDefault(),
             coordinate_validation_strategy: CoordinateValidationStrategy = CoordinateValidationStrategyDefault()
     ):
-        self.coordinate = coordinate if coordinate is not None else AttendanceCheck.DEFAULT_COORDINATE
-        self.account_id = account_id if account_id is not None else AttendanceCheck.DEFAULT_ACCOUNT_ID
         self.check_in_time_validation_strategy = check_in_time_validation_strategy
         self.check_out_time_validation_strategy = check_out_time_validation_strategy
         self.coordinate_validation_strategy = coordinate_validation_strategy
 
-    def attendance_check_in(self) -> attendance_check_type:
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(AttendanceCheck, cls).__new__(cls)
+        return cls._instance
+
+    def attendance_check_in(self, request) -> attendance_check_type:
         # 1. account_id 정보가 DB에 존재하는지 확인
-        account_info = fake_db.get_account(self.account_id)
+        account_info = fake_db.get_account(request.account_id)
         if not account_info:
             raise ValueError("유효한 계정이 아닙니다.")
 
         # 2. coordinate를 기반으로 구한 시간정보(현재 시각)이 근무 시작 예정 시간보다 0~30분 이전인지 확인
-        current_time = datetime.strptime(self.get_current_time(self.coordinate.latitude, self.coordinate.longitude),
-                                         "%H:%M:%S", )
+        current_time = datetime.strptime(
+            self.get_current_time(request.coordinate.latitude, request.coordinate.longitude),
+            "%H:%M:%S", )
         work_start_time = datetime.strptime(account_info["work_start_time"], "%H:%M:%S", )
         if self.check_in_time_validation_strategy.is_valid(current_time, work_start_time):
             raise ValueError("출근 시간이 유효하지 않습니다.")
 
         # 3. coordinate가 회사 위경도 정보에서 오차 범위 이내인지 확인
         company_coordinates = account_info["company_coordinates"]
-        if not self.coordinate_validation_strategy.is_within_error_range(company_coordinates, self.coordinate):
+        if not self.coordinate_validation_strategy.is_within_error_range(company_coordinates, request.coordinate):
             raise ValueError("유효한 위치가 아닙니다.")
 
         # 출근 기록 저장
-        fake_db.save_check_in(self.account_id, current_time)
+        fake_db.save_check_in(request.account_id, current_time)
 
         response = {
             "check_in_time": current_time.strftime("%Y-%m-%d %H:%M:%S %Z%z"),
-            "login_id": self.account_id,
-            "coordinate": {"Latitude": {self.coordinate.latitude}, "Longitude": {self.coordinate.longitude}}
+            "login_id": request.account_id,
+            "coordinate": {"Latitude": {request.coordinate.latitude}, "Longitude": {request.coordinate.longitude}}
 
         }
 
         return response
 
-    def attendance_check_out(self) -> attendance_check_type:
+    def attendance_check_out(self, reqeust) -> attendance_check_type:
         # 1. account_id 정보가 DB에 존재하는지 확인
-        account_info = fake_db.get_account(self.account_id)
+        account_info = fake_db.get_account(reqeust.account_id)
         if not account_info:
             raise ValueError("유효한 계정이 아닙니다.")
 
         # 2. coordinate를 기반으로 구한 시간정보(현재 시각)이 근무 시작 예정 시간보다 0~30분 이전인지 확인
-        current_time = datetime.strptime(self.get_current_time(self.coordinate.latitude, self.coordinate.longitude),
-                                         "%H:%M:%S", )
+        current_time = datetime.strptime(
+            self.get_current_time(reqeust.coordinate.latitude, reqeust.coordinate.longitude),
+            "%H:%M:%S", )
         work_end_time = datetime.strptime(account_info["work_end_time"], "%H:%M:%S", )
         if self.check_out_time_validation_strategy.is_valid(current_time, work_end_time):
             raise ValueError("퇴근 시간이 유효하지 않습니다.")  # todo: 퇴근은 업무시간 이전에도 가능하게 로직 변경해야 한다면 추후 리팩토링 필요
 
         # 3. coordinate가 회사 위경도 정보에서 오차 범위 이내인지 확인
         company_coordinates = account_info["company_coordinates"]
-        if not self.coordinate_validation_strategy.is_within_error_range(company_coordinates, self.coordinate):
+        if not self.coordinate_validation_strategy.is_within_error_range(company_coordinates, reqeust.coordinate):
             raise ValueError("유효한 위치가 아닙니다.")
 
             # 출근 기록 저장
-            fake_db.save_check_out(self.account_id, current_time)
+        fake_db.save_check_out(reqeust.account_id, current_time)
 
-            response = {
-                "check_in_time": current_time.strftime("%Y-%m-%d %H:%M:%S %Z%z"),
-                "login_id": self.account_id,
-                "coordinate": {"Latitude": {self.coordinate.latitude}, "Longitude": {self.coordinate.longitude}}
+        response = {
+            "check_in_time": current_time.strftime("%Y-%m-%d %H:%M:%S %Z%z"),
+            "login_id": reqeust.account_id,
+            "coordinate": {"Latitude": {reqeust.coordinate.latitude}, "Longitude": {reqeust.coordinate.longitude}}
 
-            }
+        }
 
-            return response
+        return response
 
     def get_current_time(self, latitude, longitude):
         # 위도와 경도를 사용하여 위치 정보 가져오기
